5246e820393815c7aff70c3753642af23d0970dd
Brandon Lee <brandon.lee@dshs.wa.gov>
2025-02-22 19:29:12 -0800

    Use env var for cstring

--- express/db/mongoConnect.js
+++ express/db/mongoConnect.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
 // MongoDB connection string - store this in environment variables
-//mongodb+srv://<db_username>:<db_password>@fourthrealm.adtxy.mongodb.net/?retryWrites=true&w=majority&appName=FourthRealm
-const TEST_URI = "mongodb+srv://wwwrttt:tqbfj111@fourthrealm.adtxy.mongodb.net/recipe?retryWrites=true&w=majority&appName=FourthRealm";
-
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Connect to MongoDB
 const mongoConnect = async () => {
     try {
         if (mongoose.connection.readyState !== 1) {
-            await mongoose.connect(TEST_URI, {});
+            await mongoose.connect(MONGODB_URI, {});
             console.log('MongoDB connected successfully');
         }
     } catch (error) {
